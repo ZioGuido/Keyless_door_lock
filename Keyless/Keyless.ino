31fe07dd1f686a2e48b1672e3894c61fc86ba985
Mark Miles <zioguido@gmail.com>
2019-09-02 14:28:42 +0200

    Update Keyless.ino

--- Keyless/Keyless.ino
+++ Keyless/Keyless.ino
@@ -2,14 +2,14 @@
 // Keyless StandAlone - system to open/close doors unsing a gearmotor
 // Controls MICROMOTORS E192-2S.12.49 through power driver L6201 @ 12V.
 // Runs on ATMega328P (Duemilanove, DUE, or similar boards)
-// 
+//
 // Code By Guido Scognamiglio - https://github.com/ZioGuido
 // Last update: Sept 2019
 //
 // Notes on the gear-motor MICROMOTORS E192-2S.12.49:
 // The encode reads 3 pulses every complete motor revolution.
 // The gear ratio is 49,29:1 so every gearmotor revolution = 49,29 x 3 = 147,87 pulses.
-// 
+//
 // Notes on the L6201:
 // When both inputs are set low and enable is still high, the motor stops immediately.
 // When enable is set low when the motor is spinning, power is removed abruptedly and motor slows down by inertia.
@@ -26,8 +26,8 @@
 #define PIN_SENSOR         2    // Reed switch so sense door close/open
 #define PIN_DAYNIGHT       4    // LOW = lock door if close, HIGH = do nothing
 #define PIN_MOTOR_ENABLE  10    // Controls L6201 operation
-#define PIN_MOTOR_1       12    // Motor 
-#define PIN_MOTOR_2       11    // Motor 
+#define PIN_MOTOR_1       12    // Motor
+#define PIN_MOTOR_2       11    // Motor
 #define PIN_MOTOR_HALL     3    // Hall sensor from motor encoder, to track motor revolutions
 #define PIN_LED_OK         7    // Blinks to indicates normal operation
 
@@ -35,6 +35,7 @@
 #define MOTOR_TIMEOUT     250   // Milliseconds to wait when an obstacle is found during motor movement before stopping the rotation
 #define SECURITY_TIMEOUT  3000  // Milliseconds to wait until the port is open after pushing the button, otherwise close or lock again
 #define CLOSE_WAIT        1500  // Milliseconds to wait before starting the close direction
+#define LOCK_WAIT         1000  // Milliseconds to wait before locking the door
 #define EEPROM_LOC_CONF   0     // EEPROM location where to start read/write the configuration structure
 #define EEPROM_LOC_LAST   64    // Used to remember the last motor position after a power loss
 
@@ -80,9 +81,9 @@ void StartMotor()
     motor_direction = dirOpen;
     //RequestedPulses = configuration.len_release;
 
-    // Turn until the end of coarse is found, this will beat the spring's force and keep the door open. 
+    // Turn until the limit is reached, this will beat the spring's force and keep the door open.
     // The obstacle protection will properly stop the motor.
-    RequestedPulses = 750; 
+    RequestedPulses = 750;
     break;
 
   case opRelease:
@@ -92,32 +93,37 @@ void StartMotor()
     break;
 
   case opLock:
+    // Don't lock door if it's open
+    delay(LOCK_WAIT); if (DoorStatus) return;
+    
     Serial.println("Operation: LOCK");
     motor_direction = dirClose;
     RequestedPulses = configuration.len_close;
-    if (digitalRead(PIN_J_NO_RELEASE) == LOW) RequestedPulses += configuration.len_release;
+    if (digitalRead(PIN_J_NO_RELEASE) == LOW) 
+      RequestedPulses += configuration.len_release;
     break;
 
   case opUnlock:
     Serial.println("Operation: UNLOCK");
     motor_direction = dirOpen;
     RequestedPulses = configuration.len_close;
-    if (digitalRead(PIN_J_NO_RELEASE) == LOW) RequestedPulses += configuration.len_release;
+    if (digitalRead(PIN_J_NO_RELEASE) == LOW) 
+      RequestedPulses += configuration.len_release;
     break;
   }
-  
+
   MotorIsMoving = 1;
   PrevCounter = -1;
   MotorCounter = 0;
   ObstacleTimeout = millis();
 
   digitalWrite(PIN_MOTOR_1,  motor_direction);
-  digitalWrite(PIN_MOTOR_2, !motor_direction);  
+  digitalWrite(PIN_MOTOR_2, !motor_direction);
   if (motor_direction == dirClose) delay(CLOSE_WAIT);
   digitalWrite(PIN_MOTOR_ENABLE, 1);
 
   Serial.println("START MOTOR");
-  Serial.print("motor_direction : "); Serial.println(motor_direction); 
+  Serial.print("motor_direction : "); Serial.println(motor_direction);
   Serial.println();
 }
 
@@ -128,13 +134,13 @@ void StopMotor()
   digitalWrite(PIN_MOTOR_2, 0);
 
   MotorIsMoving = 0;
-  
+
   // Disable motor driver after 100 milliseconds
-  delay(100); 
+  delay(100);
   digitalWrite(PIN_MOTOR_ENABLE, 0);
 
   PreviousMotorPosition = CurrentMotorPosition;
-  
+
   switch(LastOperation)
   {
   case opUnlock:
@@ -147,7 +153,7 @@ void StopMotor()
     CurrentMotorPosition = posReleased;
     break;
 
-  case opLock: 
+  case opLock:
     CurrentMotorPosition = posLocked;
     break;
   }
@@ -156,14 +162,14 @@ void StopMotor()
   EEPROM.write(EEPROM_LOC_LAST, CurrentMotorPosition);
 
   Serial.println("STOP MOTOR");
-  Serial.print("LastOperation : "); Serial.println(LastOperation); 
-  Serial.print("CurrentMotorPosition : "); Serial.println(CurrentMotorPosition); 
+  Serial.print("LastOperation : "); Serial.println(LastOperation);
+  Serial.print("CurrentMotorPosition : "); Serial.println(CurrentMotorPosition);
   Serial.println();
 
   LastOperation = opNothing;
 }
 
-// Called to stop motor if an obstacle has been encountered. 
+// Called to stop motor if an obstacle has been encountered.
 // Can either happen accidentally during normal operation, or intentionally during the self-calibration phase.
 void StopByObstacle()
 {
@@ -181,7 +187,7 @@ void StopByObstacle()
     CalibrationStep = calClose;
     DoCalibration();
     return;
-    
+
   case calClose:
     configuration.len_close = MotorCounter;
     CalibrationStep = calFinish;
@@ -248,19 +254,19 @@ void DoCalibration()
     ObstacleTimeout = millis();
 
     digitalWrite(PIN_MOTOR_1,  motor_direction);
-    digitalWrite(PIN_MOTOR_2, !motor_direction);  
+    digitalWrite(PIN_MOTOR_2, !motor_direction);
     if (motor_direction == dirClose) delay(CLOSE_WAIT);
     digitalWrite(PIN_MOTOR_ENABLE, 1);
   }
 }
 
-void setup() 
+void setup()
 {
   Serial.begin(115200);
 
   // Get configuration from EEPROM
   EEPROM.get(EEPROM_LOC_CONF, configuration);
-  
+
   // Motor
   pinMode(PIN_MOTOR_ENABLE, OUTPUT);
   pinMode(PIN_MOTOR_1, OUTPUT);
@@ -274,7 +280,7 @@ void setup()
   pinMode(PIN_J_RESET, INPUT_PULLUP);
   pinMode(PIN_J_ERROR_YES, INPUT_PULLUP);
   pinMode(PIN_J_NO_RELEASE, INPUT_PULLUP);
-  
+
   // Terminals
   pinMode(PIN_BUTTON, INPUT_PULLUP);
   pinMode(PIN_SENSOR, INPUT_PULLUP);
@@ -296,7 +302,7 @@ void setup()
 }
 
 
-void loop() 
+void loop()
 {
   // Read inputs
   DoorStatus        = digitalRead(PIN_SENSOR);      // LOW = door is closed
@@ -319,7 +325,7 @@ void loop()
         StopByObstacle();
     }
   }
-  
+
   // Things to do when the motor is not spinning
   else
   {
@@ -328,8 +334,8 @@ void loop()
       OpenDoor();
     }
 
-    // Lock if door is close, D&N is on and the motor is in released position
-    if (DayAndNight == HIGH && DoorStatus == LOW && CurrentMotorPosition == posReleased)
+    // Lock if motor is in released position, D&N is on, door is close
+    if (CurrentMotorPosition == posReleased && DayAndNight == HIGH && DoorStatus == LOW)
     {
       LastOperation = opLock;
       StartMotor();
@@ -344,7 +350,7 @@ void loop()
         LastOperation = opRelease;
         StartMotor();
       }
-      // If the door is still close 
+      // If the door is still close
       else
       {
         // Close or lock door if the button has been pushed but the door hasn't been open within the specified TIMEOUT
@@ -356,7 +362,7 @@ void loop()
       }
     }
   }
-  
+
   /* Don't blink...
   digitalWrite(PIN_LED_OK, HIGH);
   delay(50);
